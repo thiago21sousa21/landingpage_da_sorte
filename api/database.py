6cced12524b752cbc7c7bacba1ea72c491cdc7a2
thiago <thiago21sousa21@gmail.com>
2026-02-12 10:56:33 -0300

    ajuste para funcionar em postgres

--- api/database.py
+++ api/database.py
@@ -7,9 +7,24 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv() # Carrega as variáveis do .env
 
 # Monta a URL usando as variáveis de ambiente
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+#SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DB_URL = os.getenv("DATABASE_URL")
+
+if DB_URL and DB_URL.startswith("postgres://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+else:
+    USER = os.getenv("DB_USER")
+    PASS = os.getenv("DB_PASSWORD")
+    HOST = os.getenv("DB_HOST")
+    NAME = os.getenv("DB_NAME")
+    DB_URL = f"postgresql://{USER}:{PASS}@{HOST}/{NAME}"
+
+
+
+    
+
+engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
